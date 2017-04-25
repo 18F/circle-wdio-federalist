6a338d22de0b287c6b60c045e7cbd5bce2cb7a20
Shawn Allen <shawn.allen@gsa.gov>
2017-04-25 10:20:27 -0700

    use new federalist URLs

--- test/ci.sh
+++ test/ci.sh
@@ -3,9 +3,9 @@
 repo="${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
 
 if [ "$CIRCLE_BRANCH" == "master" ]; then
-  url="http://federalist.18f.gov.s3-website-us-east-1.amazonaws.com/site/${repo}"
+  url="https://federalist-proxy.app.cloud.gov/site/${repo}"
 else
-  url="https://federalist.18f.gov/preview/${repo}/${CIRCLE_BRANCH}"
+  url="https://federalist.fr.cloud.gov/preview/${repo}/${CIRCLE_BRANCH}"
 fi
 
 echo "testing site URL: ${url}"
